ec644213fad69b507435079bede374e7541d440e
@code <jj4068851@gmail.com>
2023-12-11 12:12:18 +0700

    Add: added transition button navigation when scrolling

--- src/components/button/buttonNavigation.jsx
+++ src/components/button/buttonNavigation.jsx
@@ -2,10 +2,21 @@ import { React, useEffect, useState } from "react"
 import { Link, Outlet, useLocation } from "react-router-dom"
 import { selectedButtonNavigate } from "@/utils/selectedButtonNavigate"
 import { toggleDarkMode } from "@/utils/darkmode"
+import { cn } from "cntw"
 
 function ButtonNavigation() {
     const { pathname } = useLocation();
-    useEffect(() => selectedButtonNavigate(pathname), [pathname])
+    const [currentOffsetY, setCurrentOffsetY] = useState("0");
+    const [hiddenButtonNavigation, setHiddenButtonNavigation] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", function () {
+            setCurrentOffsetY(() => window.scrollY);
+            setHiddenButtonNavigation(window.scrollY > currentOffsetY)
+        })
+        selectedButtonNavigate(pathname);
+
+    }, [pathname, currentOffsetY])
+    console.log(currentOffsetY, hiddenButtonNavigation)
     const [isDarkMode, setIsDarkMode] = useState(false);
     const handleDarkMode = () => {
         setIsDarkMode(!isDarkMode);
@@ -13,7 +24,7 @@ function ButtonNavigation() {
     }
     return (
         <>
-            <div className="bg-transparent w-full h-auto flex justify-center absolute inset-x-0 bottom-8">
+            <div className={cn("bg-transparent w-full h-auto flex justify-center fixed inset-x-0 bottom-8 transition-all ease-in-out", hiddenButtonNavigation && "-mb-40")}>
                 <div className="bg-white dark:bg-slate-800 button-navigation h-auto flex justify-around items-center rounded-lg border-2 px-5 py-5 mx-5">
                     <Link to="/" className="dark:text-slate-400">
                         <section className="flex flex-col justify-center items-center">
@@ -48,17 +59,17 @@ function ButtonNavigation() {
                         </section>
                     </Link>
                     <button className="current-selected flex flex-col justify-center items-center" onClick={handleDarkMode}>
-                        {isDarkMode ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        {isDarkMode ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="icon transition ease-in-out delay-15 animate-bounce">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
                         </svg> :
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="icon transition ease-in-out delay-15 animate-bounce">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
                             </svg>}
 
                         <span>Mode</span>
                     </button>
                 </div>
-            </div>
+            </div >
             <Outlet />
         </>
     )
