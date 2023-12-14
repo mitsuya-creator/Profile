c99069526a5a5c2fde77d04fc1c17d1915690782
takachan0012 <jj4068851@gmail.com>
2023-12-14 11:46:10 +0700

    Reformat: reformat style margin

--- src/components/education.jsx
+++ src/components/education.jsx
@@ -3,7 +3,7 @@ import Timeline from "@/components/shared/timeline"
 
 function Education() {
     return (
-        <div className="w-screen flex justify-center mt-1">
+        <div className="w-screen flex justify-center mt-11 md:mt-1">
             <div className="w-11/12 h-full xl:w-8/12">
                 <h3 className="text-lg font-bold mb-3 md:text-xl dark:text-slate-100">Education & Course</h3>
                 <Timeline/>
