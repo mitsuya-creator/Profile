05bba9dcc75e0362c738e48b9b11627347e569eb
takachan0012 <jj4068851@gmail.com>
2023-12-14 11:24:09 +0700

    Add: initial data

---
+++ src/utils/educationData.js
@@ -0,0 +1,23 @@
+const educationsData = [
+    {
+        id: 0,
+        title: "Vocational high school 1 Sukaluyu, West Java",
+        description: "As an alumnus of a vocational high school specializing in computer networks, I've delved deep into the intricate web of technology. My education equipped me with comprehensive knowledge, from mastering MikroTik to navigating switches, IPs, UDP, and providing meticulous computer service maintenance.",
+        years: "2017--2020"
+    },
+    {
+        id: 1,
+        title: "Learn to Create Web Applications with React, Beginner at Dicoding",
+        description:"Embarking on my journey with React, I've cultivated a robust skill set that revolves around its fundamental pillars. From mastering elements and crafting versatile components to harnessing the power of props for seamless data flow, handling events dynamically, and orchestrating application states ",
+        years: "2023--2023"
+    },
+    {
+        id: 2,
+        title: "Learning Path JavaScript Expert, at Codepolitan",
+        description: "My journey toward becoming a JavaScript expert has been an immersive exploration through the foundational elements and advanced concepts that power the digital realm. From mastering the core trio of web development - HTML, CSS, and JavaScript - to delving into the intricate workings of the JavaScript DOM and its asynchronous nature, I've navigated the intricacies of handling data through AJAX Web APIs with precision. I can't describe it further yet, because I'm still learning on this path.",
+        years: "2023--present"
+    }
+]
+
+
+export {educationsData}
