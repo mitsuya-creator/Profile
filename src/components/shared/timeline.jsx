6c7cfa1fc6052a0a178f0b80af380dc36efeefbb
takachan0012 <jj4068851@gmail.com>
2023-12-14 10:29:15 +0700

    Add: Added component timeline

---
+++ src/components/shared/timeline.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+function Timeline() {
+    return (
+        <div className="w-full h-full border-s-2 border-violet-800 dark:text-slate-400">
+            <div className="flex justify-between">
+                <section
+                    className="bg-violet-800 w-11 h-11 rounded-full ms-2 flex justify-center items-center">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 fill-slate-100 dark:fill-slate-200">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"/>
+                    </svg>
+                </section>
+                <section className="w-11/12 h-full p-6 bg-violet-800 relative rounded ms-3 text-slate-100">
+                    <div
+                        className="w-6 h-6 bg-violet-800 absolute rotate-45 top-3 -left-1 md:-left-2"></div>
+                    <h2 className="font-bold">Vocational high school 1 Sukaluyu, West Java
+                    </h2>
+                    <p className="my-3">As an alumnus of a vocational high school specializing in computer networks, I've delved deep into the intricate web of technology. My education equipped me with comprehensive knowledge, from mastering MikroTik to navigating switches, IPs, UDP, and providing meticulous computer service maintenance.</p>
+                        <span>2017 -- 2020</span>
+                </section>
+            </div>
+        </div>
+    )
+}
+
+function ComponentsTimeline(){
+    
+}
+
+export default Timeline;
