b8e57861bb6a009664002f139c0641d0aa079b1f
takachan0012 <jj4068851@gmail.com>
2023-12-18 12:00:25 +0700

    Add: added cv components

--- src/components/curiculumVitae.jsx
+++ src/components/curiculumVitae.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import profile from "@/assets/profile.png"
+import {educationsData} from "@/utils/educationData"
 
 function CurriculumVitae(){
     return(
@@ -9,7 +10,7 @@ function CurriculumVitae(){
                     <img src={profile} alt="profile-image" width={100} />
                 </div>
                 <div>
-                    <h1 className="text-xl font-bold">Jujun Junaedi</h1>
+                    <h1 className="text-2xl font-bold">Jujun Junaedi</h1>
                     <p className="flex flex-col">
                         <span>Cianjur, 11 July 2001</span>
                         <span>jujunjunaedi110107@gmail.com</span>
@@ -20,6 +21,21 @@ function CurriculumVitae(){
             <section className="w-11/12 flex justify-evenly mt-3 items-center border-b p-4 border-black">
                 <p>I am a passionate tech enthusiast and a proud alum of a vocational high school specializing in computer network technology. My journey into the intricate world of programming began here, where I discovered my love for unraveling complexities and weaving them into seamless connections.</p>
             </section>
+            <section className="w-11/12 flex flex-col mt-3 border-b p-4 border-black">
+                <h2 className="text-xl font-bold">Educational Background</h2>
+                {educationsData.map(data => <LisContent key={data.id} title={data.title} description={data.description} years={data.years}/>)}
+            </section>
+        </div>
+    )
+}
+
+function LisContent({title,description,years}){
+    return(
+        <div className="mt-7">
+            <h2 className="font-bold">{title}
+            </h2>
+            <p className="my-3">{description}</p>
+            <span className="italic">{years}</span>
         </div>
     )
 }
