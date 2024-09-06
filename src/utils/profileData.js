5c3e175cf4e8987b9fe6cbf7289f57cf42d2abbc
takachan0012 <jj4068851@gmail.com>
2024-09-06 21:58:06 +0700

    feat: reverse order

--- src/utils/profileData.js
+++ src/utils/profileData.js
@@ -1,9 +1,9 @@
 const educationData = [
     {
-        id: 0,
-        title: "Vocational high school 1 Sukaluyu, West Java",
-        description: "As an alumnus of a vocational high school specializing in computer networks, I've delved deep into the intricate web of technology. My education equipped me with comprehensive knowledge, from mastering MikroTik to navigating switches, IPs, UDP, and providing meticulous computer service maintenance.",
-        years: "July 2017 to June 2020"
+        id: 2,
+        title: "Learning Path JavaScript Expert, at Codepolitan",
+        description: "My journey toward becoming a JavaScript expert has been an immersive exploration through the foundational elements and advanced concepts that power the digital realm. From mastering the core trio of web development - HTML, CSS, and JavaScript - to delving into the intricate workings of the JavaScript DOM and its asynchronous nature, I've navigated the intricacies of handling data through AJAX Web APIs with precision. I can't describe it further yet, because I'm still learning on this path.",
+        years: "November 2023 to Present"
     },
     {
         id: 1,
@@ -12,10 +12,10 @@ const educationData = [
         years: "September 2023 to November 2023"
     },
     {
-        id: 2,
-        title: "Learning Path JavaScript Expert, at Codepolitan",
-        description: "My journey toward becoming a JavaScript expert has been an immersive exploration through the foundational elements and advanced concepts that power the digital realm. From mastering the core trio of web development - HTML, CSS, and JavaScript - to delving into the intricate workings of the JavaScript DOM and its asynchronous nature, I've navigated the intricacies of handling data through AJAX Web APIs with precision. I can't describe it further yet, because I'm still learning on this path.",
-        years: "November 2023 to Present"
+        id: 0,
+        title: "Vocational high school 1 Sukaluyu, West Java",
+        description: "As an alumnus of a vocational high school specializing in computer networks, I've delved deep into the intricate web of technology. My education equipped me with comprehensive knowledge, from mastering MikroTik to navigating switches, IPs, UDP, and providing meticulous computer service maintenance.",
+        years: "July 2017 to June 2020"
     }
 ]
 
