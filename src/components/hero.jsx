159963cc51f628dace9b2d5b155de2679f1ce694
takachan0012 <jj4068851@gmail.com>
2024-09-06 21:54:12 +0700

    feat: update profile picture

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import profileImage from "@/assets/profile.png"
+import profileImage from "@/assets/profile.jpg"
 import { Link } from "react-router-dom"
 
 function Hero() {
